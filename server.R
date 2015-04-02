d8b548915b6f8a581898670065dbe96cd3776703
Ari Lamstein <arilamstein@gmail.com>
2015-04-01 18:18:19 -0700

    get d map 2 show

--- server.R
+++ server.R
@@ -1,23 +1,12 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(choroplethrZip)
 
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
+  output$map <- renderPlot({
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    data(df_pop_zip)
+    zip_choropleth(df_pop_zip, msa_zoom=input$msa)
   })
 
 })
