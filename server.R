d81af9264012c412568bebe9ade673a3baca7158
Ari Lamstein <arilamstein@gmail.com>
2015-04-02 22:51:03 -0700

    zips update based on county selections

--- server.R
+++ server.R
@@ -8,7 +8,7 @@ shinyServer(function(input, output) {
   output$map = renderPlot({
     df_demographics$value = df_demographics[, input$value]
     num_colors = as.numeric(input$num_colors)
-    zip_choropleth(df_demographics, num_colors=num_colors, msa_zoom=input$msa)
+    zip_choropleth(df_demographics, num_colors=num_colors, zip_zoom=input$zips)
   })
   
   output$counties = renderUI({ 
@@ -18,7 +18,10 @@ shinyServer(function(input, output) {
   })
 
   output$zips = renderUI({ 
-    if (input$counties == get_all_counties_in_msa(input$msa)) {
+    x=as.numeric(input$counties)
+    y=get_all_counties_in_msa(input$msa)
+    names(y)=NULL
+    if (identical(x,y)) {
       zcta_list = get_all_zip_in_msa(input$msa)
     } else {
       zcta_list = get_zctas_in_counties(input$counties)
@@ -33,10 +36,17 @@ get_all_counties_in_msa = function(msa)
 {
   county_list = zip.regions %>%
     filter(cbsa.title == msa) %>%
-    select(county.name) %>%
+    select(county.fips.numeric, county.name) %>%
     unique()
-  county_list$county.name
+  ret = county_list$county.fips.numeric
+  names(ret) = county_list$county.name
+  ret
 }
+
+county_list = zip.regions %>%
+  filter(cbsa.title == "Abilene, TX") %>%
+  select(county.fips.numeric, county.name) %>%
+  unique()
 
 get_all_zip_in_msa = function(msa)
 {
@@ -49,4 +59,10 @@ get_all_zip_in_msa = function(msa)
 
 get_zctas_in_counties = function(counties)
 {
+  counties = as.numeric(counties)
+  zctas = zip.regions %>%
+    filter(county.fips.numeric %in% counties) %>%
+    select(region) %>%
+    unique()
+  zctas$region
 }
